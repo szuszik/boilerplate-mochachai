d65af2e7a2ecf25ea7b937bca77e3267ba367ee5
szuszik <szuszi.rofled@gmail.com>
2022-02-20 23:38:51 +0100

    feat: finish tests for api

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -67,11 +67,16 @@ suite('Functional Tests', function () {
 });
 
 const Browser = require('zombie');
+Browser.site = 'https://szuszik-mochachai.herokuapp.com/' //My page
 
 suite('Functional Tests with Zombie.js', function () {
   this.timeout(5000);
 
+  const browser = new Browser()
 
+  suiteSetup(function(done){
+    return browser.visit('/', done)
+  })
 
   suite('Headless browser', function () {
     test('should have a working "site" property', function() {
@@ -82,15 +87,27 @@ suite('Functional Tests with Zombie.js', function () {
   suite('"Famous Italian Explorers" form', function () {
     // #5
     test('Submit the surname "Colombo" in the HTML form', function (done) {
-      assert.fail();
-
-      done();
+      browser.fill('surname', 'Colombo').then(() => {
+        browser.pressButton('submit', () => {
+          browser.assert.success()
+          browser.assert.text('span#name', 'Cristoforo')
+          browser.assert.text('span#surname', 'Colombo')
+          browser.assert.elements('span#dates', 1)
+          done()
+        })
+      })
     });
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
-      assert.fail();
-
-      done();
+      browser.fill('surname', 'Vespucci').then(() => {
+        browser.pressButton('submit', () => {
+          browser.assert.success()
+          browser.assert.text('span#name', 'Amerigo')
+          browser.assert.text('span#surname', 'Vespucci')
+          browser.assert.elements('span#dates', 1)
+          done()
+        })
+      })
     });
   });
 });
